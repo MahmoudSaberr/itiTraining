71b5419b8512358d4cebb20bf7f813466d2ff720
Mahmoud Saber <saberm19071911@gmail.com>
2022-08-30 00:17:09 +0200

    S2SecondTaskScreen S2ThirdTaskScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:iti_training/modules/section_2/second_task/s2_second_task_screen.dart';
+import 'package:iti_training/modules/section_2/third_task/s2_third_task_screen.dart';
 
 import 'modules/section_2/first_task/s2_first_task_screen.dart';
 
--- lib/modules/section_2/third_task/s2_third_task_screen.dart
+++ lib/modules/section_2/third_task/s2_third_task_screen.dart
@@ -5,6 +5,40 @@ class S2ThirdTaskScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar:AppBar(
+        title: Text("Android ATC Pizza Place"),
+      ),
+      body: Container(
+        width: double.infinity,
+        height: 100,
+        margin:EdgeInsets.all(8.0),
+        child: Card(  
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(8.0))),
+          color: Colors.blueAccent,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const ListTile(
+                leading: CircleAvatar(
+                  maxRadius: 60,
+                  backgroundImage: NetworkImage(
+                      'https://images.unsplash.com/photo-1513104890138-7c749659a591?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80'
+                  ),
+                ),
+                title: Text(
+                  'Vegetable Pizza',
+                  style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                  color: Colors.white
+              ),
+            ),
+          ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
