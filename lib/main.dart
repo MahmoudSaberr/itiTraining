c76803eb0954cd106ea1896529df768635249f0c
Mahmoud Saber <saberm19071911@gmail.com>
2022-08-29 18:43:01 +0200

    Task4

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:iti_training/modules/first_task/first_task_screen.dart';
-import 'package:iti_training/modules/second_task/second_task_screen.dart';
 import 'package:iti_training/modules/section_2/second_task/s2_second_task_screen.dart';
 
 import 'modules/section_2/first_task/s2_first_task_screen.dart';
---
+++ lib/modules/section_2/third_task/s2_third_task_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class S2ThirdTaskScreen extends StatelessWidget {
+  const S2ThirdTaskScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
