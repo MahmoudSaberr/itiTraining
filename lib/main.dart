d9f0daf9be7c3ca753e36723217389318407370b
Mahmoud Saber <saberm19071911@gmail.com>
2022-08-30 00:28:47 +0200

    S2SecondTaskScreen S2ThirdTaskScreen

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const S2SecondTaskScreen(),
+      home: const S2ThirdTaskScreen(),
     );
   }
 }
--- lib/modules/section_2/third_task/s2_third_task_screen.dart
+++ lib/modules/section_2/third_task/s2_third_task_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:iti_training/modules/section_2/second_task/s2_second_task_screen.dart';
 
 class S2ThirdTaskScreen extends StatelessWidget {
   const S2ThirdTaskScreen({Key? key}) : super(key: key);
@@ -13,29 +14,37 @@ class S2ThirdTaskScreen extends StatelessWidget {
         width: double.infinity,
         height: 100,
         margin:EdgeInsets.all(8.0),
-        child: Card(  
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(8.0))),
-          color: Colors.blueAccent,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const ListTile(
-                leading: CircleAvatar(
-                  maxRadius: 60,
-                  backgroundImage: NetworkImage(
-                      'https://images.unsplash.com/photo-1513104890138-7c749659a591?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80'
+        child: GestureDetector(
+          onTap:  () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const S2SecondTaskScreen()),
+            );
+          },
+          child: Card(
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(8.0))),
+            color: Colors.blueAccent,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const ListTile(
+                  leading: CircleAvatar(
+                    maxRadius: 60,
+                    backgroundImage: NetworkImage(
+                        'https://images.unsplash.com/photo-1513104890138-7c749659a591?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80'
+                    ),
                   ),
+                  title: Text(
+                    'Vegetable Pizza',
+                    style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                    color: Colors.white
                 ),
-                title: Text(
-                  'Vegetable Pizza',
-                  style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18,
-                  color: Colors.white
               ),
             ),
-          ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
